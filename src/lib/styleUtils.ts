8a67d2b1beeccb7e2a4c22c53a03839d906398d2
hanyoungjae <han6318@naver.com>
2020-10-11 14:02:49 +0900

    task: implement TextInput component

--- src/lib/styleUtils.ts
+++ src/lib/styleUtils.ts
@@ -1,6 +1,13 @@
 import memoizeOne from 'memoize-one';
+import { css } from 'styled-components';
 
 export const boxShadow = memoizeOne((blurLevel: number = 0) => {
   const boxShadowBlurPixel = (blurLevel * 2 + 3) * 2;
   return `box-shadow: 0 4px ${boxShadowBlurPixel}px 0 rgba(0, 0, 0, 0.37)`;
 });
+
+export const focusedInputStyle = css`
+  outline: none;
+  border: 1px solid #5d95ff;
+  transition: border 0.25s ease;
+`;
